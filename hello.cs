cfc586f8d7cbe414ba974a8919b7d6974f90474b
OLAWALE1989 <92593117+OLAWALE1989@users.noreply.github.com>
2021-10-28 17:45:23 +0100

    first commit

---
+++ hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace firstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        
+        }
+    }
+}
